b2276b12635126e24d3d5063dea180b4bfd1193c
Jace Halvorson <jacehalvorson33@gmail.com>
2024-06-08 20:33:27 -0500

    Enable IRQs from RTC and GPIO
    Abstract pin intializations, restructure wait-for-interrupt method, add logic for toggling shades back and forth

--- shades.c
+++ shades.c
@@ -4,59 +4,101 @@
 #include "pico/util/datetime.h"
 #include "hardware/sync.h"
 
-#define LED_PIN PICO_DEFAULT_LED_PIN
-#define BUTTON_PIN 3
+#define MOTOR_DURATION_MS             2400
 
-bool is_LED_on = false;
+#define LED_PIN                       PICO_DEFAULT_LED_PIN
+#define CLOCKWISE_PIN                 0
+#define COUNTER_CLOCKWISE_PIN         1
+#define MOTOR_ENABLE_PIN              2
+#define BUTTON_PIN                    3
+#define BUTTON_ENABLE_PIN             22
 
-const datetime_t alarm_time = {
+#define NUM_PINS 6
+
+const int pin_definitions[NUM_PINS][3] = {
+    {LED_PIN,                   GPIO_OUT, 0},
+    {BUTTON_ENABLE_PIN,         GPIO_OUT, 1},
+    {CLOCKWISE_PIN,             GPIO_OUT, 0},
+    {COUNTER_CLOCKWISE_PIN,     GPIO_OUT, 0},
+    {MOTOR_ENABLE_PIN,          GPIO_OUT, 1},
+    {BUTTON_PIN,                GPIO_IN, 0},
+};
+
+datetime_t alarm_time = {
     .year  = -1,
     .month = -1,
     .day   = -1,
     .dotw  = -1, // 0 is Sunday
-    .hour  = -1,
-    .min   = -1,
-    .sec   = 2
+    .hour  = 7,
+    .min   = 0,
+    .sec   = 0
 };
 
-void alarm_callback(void) {
-    is_LED_on = !is_LED_on;
-    gpio_put(LED_PIN, is_LED_on);
+void irq_callback(void) {
+    gpio_put(LED_PIN, 1);
 }
 
 void gpio_callback(uint gpio, uint32_t events) {
-    is_LED_on = !is_LED_on;
-    gpio_put(LED_PIN, is_LED_on);
+    gpio_put(LED_PIN, 1);
 }
 
 int main() {
+    int motor_direction_pin;
+
     // Start on Friday 5th of June 2020 15:45:00
     datetime_t t = {
-            .year  = 2020,
+            .year  = 2024,
             .month = 06,
-            .day   = 05,
-            .dotw  = 5, // 0 is Sunday, so 5 is Friday
-            .hour  = 15,
-            .min   = 45,
+            .day   = 06,
+            .dotw  = 4, // 0 is Sunday
+            .hour  = 23,
+            .min   = 19,
             .sec   = 00
     };
 
     // Start the RTC
-    // rtc_init();
-    // rtc_set_datetime(&t);
+    rtc_init();
+    rtc_set_datetime(&t);
 
-    // LED pin (output)
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // Initialize pins
+    for (int i = 0; i < NUM_PINS; i++)
+    {
+        int pin = pin_definitions[i][0];
+        int dir = pin_definitions[i][1];
+        int val = pin_definitions[i][2];
 
-    // Button (input)
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+        gpio_init(pin);
+        gpio_set_dir(pin, dir);
+        if (val == GPIO_OUT)
+            gpio_put(pin, GPIO_OUT);
+    }
 
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    // Initialize shades to be closed
+    bool shades_closed = true;
 
-    while (1);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    rtc_set_alarm((datetime_t *)&alarm_time, &irq_callback);
 
-    // rtc_set_alarm((datetime_t *)&alarm_time, &alarm_callback);
-    // __wfi();
+    while (1)
+    {
+        // Wait here until IRQ fires (either button or alarm)
+        __wfi();
+
+        if (shades_closed)
+            motor_direction_pin = CLOCKWISE_PIN;
+        else
+            motor_direction_pin = COUNTER_CLOCKWISE_PIN;
+
+        // Power the motor for TBD seconds
+        gpio_put(motor_direction_pin, 1);
+        sleep_ms(MOTOR_DURATION_MS);
+        gpio_put(motor_direction_pin, 0);
+
+        shades_closed = motor_direction_pin;
+
+        // Set alarm for 7 AM
+        rtc_set_alarm((datetime_t *)&alarm_time, &irq_callback);
+
+        gpio_put(LED_PIN, 0);
+    }
 }
